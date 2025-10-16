90691c0a4102e255e0ec4365b53ade41e6e01b6d
WilmarParedesRomero <U25209113@utp.edu.pe>
2025-10-16 02:11:45 -0500

    Update DATABASE UniversidadDB.sql

--- DATABASE UniversidadDB.sql
+++ DATABASE UniversidadDB.sql
@@ -194,16 +194,147 @@ FROM Alumnos
 WHERE Edad > 20
 ORDER BY Edad DESC;
 
+-- Agregar nuevas carreras
+INSERT INTO Carreras (NombreCarrera, DuracionAnios)
+VALUES ('Ingeniería de Software', 5),
+       ('Psicología', 5);
 
+-- Agregar nuevos alumnos
+INSERT INTO Alumnos (Nombre, Apellido, Edad, Universidad, Ciclo, CarreraID)
+VALUES ('Christian', 'Morales', 21, 'Universidad Nacional', '5to Ciclo', 1),
+       ('Lesly', 'Ramos', 20, 'Universidad Nacional', '4to Ciclo', 2);
 
+-- Agregar profesor
+INSERT INTO Profesores (Nombre, Especialidad)
+VALUES ('Héctor Gómez', 'Cálculo');
 
+-- Agregar curso
+INSERT INTO Cursos (NombreCurso, Creditos, ProfesorID)
+VALUES ('Cálculo Avanzado', 5, 1);
 
+-- Agregar aula
+INSERT INTO Aulas (NombreAula, Capacidad)
+VALUES ('A101', 40);
 
+-- Agregar matrícula
+INSERT INTO Matriculas (AlumnoID, CursoID, FechaMatricula)
+VALUES (1, 1, GETDATE());
 
+-- Agregar nota
+INSERT INTO Notas (MatriculaID, Nota)
+VALUES (1, 18.5);
 
+-- Cambiar la nota del alumno matriculado con ID 1
+UPDATE Notas
+SET Nota = 19.0
+WHERE MatriculaID = 1;
 
+-- Cambiar el ciclo de Christian a "6to Ciclo"
+UPDATE Alumnos
+SET Ciclo = '6to Ciclo'
+WHERE Nombre = 'Christian';
 
+-- Cambiar el estado de la matrícula a "Finalizado"
+UPDATE Matriculas
+SET Estado = 'Finalizado'
+WHERE MatriculaID = 1;
 
+-- Eliminar una nota baja
+DELETE FROM Notas
+WHERE Nota < 12;
 
+-- Eliminar un curso que ya no se dicta
+DELETE FROM Cursos
+WHERE NombreCurso = 'Historia Contemporánea';
 
+-- TRY--
+USE UniversidadDB;
+GO
+
+BEGIN TRY
+    BEGIN TRANSACTION; -- Inicia la transacción
+
+    PRINT 'Iniciando proceso de matrícula...';
+
+    -- Insertar un nuevo alumno
+    INSERT INTO Alumnos (Nombre, Apellido, Edad, Universidad, Ciclo, CarreraID)
+    VALUES ('Camila', 'Herrera', 19, 'Universidad Nacional', 2, 1);
+
+    DECLARE @NuevoAlumnoID INT = SCOPE_IDENTITY();  -- Captura el ID del alumno recién insertado
+
+    PRINT 'Alumno insertado correctamente con ID: ' + CAST(@NuevoAlumnoID AS NVARCHAR(10));
+
+    --  Registrar su matrícula en un curso existente
+    INSERT INTO Matriculas (AlumnoID, CursoID, FechaMatricula, Estado)
+    VALUES (@NuevoAlumnoID, 2, GETDATE(), 'Activo');  -- CursoID 2 = Programación I
+
+    DECLARE @NuevaMatriculaID INT = SCOPE_IDENTITY();
+
+    PRINT 'Matrícula creada con ID: ' + CAST(@NuevaMatriculaID AS NVARCHAR(10));
+
+    --  Registrar una nota inicial para esa matrícula
+    INSERT INTO Notas (MatriculaID, Nota)
+    VALUES (@NuevaMatriculaID, 17.5);
+
+    PRINT 'Nota registrada correctamente.';
+
+    --  Si todo sale bien, se confirma la transacción
+    COMMIT TRANSACTION;
+    PRINT 'Transacción completada exitosamente.';
+
+END TRY
+BEGIN CATCH
+    --  Si ocurre un error, se revierte todo
+    PRINT 'Error detectado. Revirtiendo cambios...';
+    ROLLBACK TRANSACTION;
+
+    PRINT 'Mensaje de error: ' + ERROR_MESSAGE();
+END CATCH;
+GO
+-- Iniciando proceso de matrícula...
+-- Alumno insertado correctamente con ID: 9
+-- Matrícula creada con ID: 9
+-- Nota registrada correctamente.
+-- Transacción completada exitosamente.
+
+ -- Tabla de   Relaciones 
+1:N Carreras → Alumnos
+1:N Profesores → Cursos
+1:N Cursos → Matrículas
+1:N Alumnos → Matrículas
+1:N Matrículas → Notas
+N:M Alumnos ↔ Cursos
+------------------------
+    
+USE UniversidadDB;
+GO
+
+SELECT 
+    a.AlumnoID,
+    a.Nombre + ' ' + a.Apellido AS Alumno,
+    m.MatriculaID,
+    m.CursoID,
+    m.Estado
+FROM Alumnos a
+LEFT JOIN Matriculas m
+    ON a.AlumnoID = m.AlumnoID;
+-- Muestra en pantalla 
+--AlumnoID	Alumno	MatriculaID	CursoID	Estado
+--1	Luis Torres	1	1	Activo
+--2	Andrea Gómez	2	2	Activo
+--9	Camila Herrera	NULL	NULL	NULL
+
+SELECT 
+    c.CursoID,
+    c.NombreCurso,
+    m.MatriculaID,
+    m.AlumnoID
+FROM Matriculas m
+RIGHT JOIN Cursos c
+    ON m.CursoID = c.CursoID;
+-- Muestra en pantalla 
+--CursoID	NombreCurso	MatriculaID	AlumnoID
+--1	Álgebra Lineal	1	1
+--2	Programación I	2	2
+--9	Filosofía Moderna	NULL	NULL
 
