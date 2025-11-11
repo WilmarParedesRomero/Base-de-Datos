264274b33dd7ebe386b2105dbe398936a546e5c5
WilmarParedesRomero <U25209113@utp.edu.pe>
2025-11-10 23:46:11 -0500

    Update DATABASE UniversidadDB.sql

--- DATABASE UniversidadDB.sql
+++ DATABASE UniversidadDB.sql
@@ -1,7 +1,7 @@
 -- Crear la base de datos
 CREATE DATABASE UniversidadDB;
 GO
-     
+      
 -- Usar la base de datos  
 USE UniversidadDB; 
 GO
@@ -337,6 +337,7 @@ RIGHT JOIN Cursos c
 --1	Álgebra Lineal	1	1
 --2	Programación I	2	2
 --9	Filosofía Moderna	NULL	NULL
+
 
 
 
